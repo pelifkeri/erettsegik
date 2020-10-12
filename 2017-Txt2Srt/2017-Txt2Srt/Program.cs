7ca8ce82e0d340173452330b47a403a03b1c2902
Peli, Ferenc <ferenc.peli@accenture.com>
2020-10-12 18:26:15 +0200

    Adding blank solution

---
+++ 2017-Txt2Srt/2017-Txt2Srt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2017_Txt2Srt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
