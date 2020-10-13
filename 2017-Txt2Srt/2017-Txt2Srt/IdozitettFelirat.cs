39baa13b789f5afb218d06916f147a76aa2012b0
Peli, Ferenc <ferenc.peli@accenture.com>
2020-10-13 23:27:43 +0200

    Completed solution

---
+++ 2017-Txt2Srt/2017-Txt2Srt/IdozitettFelirat.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _2017_Txt2Srt
+{
+    public class IdozitettFelirat
+    {
+        public string Timing { get; set; }
+        public string Subtitle { get; set; }
+
+        public int SzavakSzama => Subtitle.Split(' ').Length;
+
+        public string SrtIdozites
+        {
+            get
+            {
+                var first = GetSecondsFromString(Timing.Split('-')[0].Trim());
+                var second = GetSecondsFromString(Timing.Split('-')[1].Trim());
+
+                return $"{GetTimeStringFromSeconds(first)} ---> {GetTimeStringFromSeconds(second)}";
+            }
+        }
+
+        public IdozitettFelirat(string timing, string subtitle)
+        {
+            Timing = timing;
+            Subtitle = subtitle;
+        }
+
+        private int GetSecondsFromString(string time)
+        {
+            var split = time.Split(':');
+            return int.Parse(split[0]) * 60 + int.Parse(split[1]);
+        }
+
+        private string GetTimeStringFromSeconds(int seconds)
+        {
+            TimeSpan time = TimeSpan.FromSeconds(seconds);
+            return time.ToString(@"hh\:mm\:ss");
+        }
+    }
+}
--- 2017-Txt2Srt/2017-Txt2Srt/Program.cs
+++ 2017-Txt2Srt/2017-Txt2Srt/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace _2017_Txt2Srt
 {
-    class Program
+    public class Program
     {
+        public static List<IdozitettFelirat> list = new List<IdozitettFelirat>();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Task4();
+            Console.WriteLine($"5. feladat - Feliratok száma: {list.Count}");
+            Task7();
+            Task9();
+
+            Console.ReadLine();
+        }
+
+        static void Task4()
+        {
+            string[] lines = File.ReadAllLines("feliratok.txt");
+            for (int i = 0; i < lines.Length - 1; i += 2)
+            {
+                list.Add(new IdozitettFelirat(lines[i], lines[i + 1]));
+            }
+        }
+
+        static void Task7()
+        {
+            string longest = list.OrderBy(x => x.SzavakSzama).Last().Subtitle;
+            Console.WriteLine($"7. feladat - Legtöbb szóból álló felirat: {longest}");
+        }
+
+        static void Task9()
+        {
+            using StreamWriter sw = new StreamWriter("felirat.srt");
+            foreach (var sub in list)
+            {
+                sw.Write($"{list.IndexOf(sub) + 1}\n{sub.SrtIdozites}\n{sub.Subtitle}\n\n");
+            }
         }
     }
 }
