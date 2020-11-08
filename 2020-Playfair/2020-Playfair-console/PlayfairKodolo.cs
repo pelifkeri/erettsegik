cf47160eef5fda7a0ef4d879aae5761548f1b9f7
Peli, Ferenc <ferenc.peli@accenture.com>
2020-11-08 19:28:35 +0100

    Adding empty playfair solution

---
+++ 2020-Playfair/2020-Playfair-console/PlayfairKodolo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2020_Playfair_console
+{
+    public class PlayfairKodolo
+    {
+        public PlayfairKodolo(string fileName)
+        {
+
+        }
+    }
+}
